3cce28747414d5224c1bc452fbf134dc7f7d0fc1
GibboK <gibbok.coding@gmail.com>
2017-08-29 08:42:32 +0200

    Add load data from json

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import MovieList from './MovieList.js'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ui: {},
+      data: {}
+    };
+  }
+  componentDidMount() {
+    fetch('./data.json').then(response => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ', response.status);
+        return;
+      }
+      response.json().then(data => {
+        console.log(data);
+        this.setState({ data });
+      });
+    }).catch(error => {
+      console.log('Error with fetch', error);
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +36,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MovieList data={this.state.data} />
       </div>
     );
   }
